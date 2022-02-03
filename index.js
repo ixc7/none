5f5111c81dcb1f91d895205508a83c5cf8f5e3f6
ixc7 <isaacxpreston7@gmail.com>
2022-02-03 00:27:40 -0500

    tput sc rc

--- index.js
+++ index.js
@@ -5,44 +5,61 @@ const env = {
   maxHeight: process.stdout.rows,
   quitKey: 'q',
   gotoBottom: `\x1b[${process.stdout.rows};1H`,
-  gotoTop: `\x1b[1;1H`,
+  gotoTop: '\x1b[1;1H',
   gotoLeft: `\x1B[${process.stdout.columns}D`,
   gotoRight: `\x1B[${process.stdout.columns}C`,
+  jumpBackspace: '\x1B\b',
+  sc: '\x1b7',
+  rc: '\x1b8', 
+  // rc ok w down/up arrows 
+  // doesnt work w newline
+  newline: '\r\n',
+  // newline: '\n'
+  tab: '\t',
+  // unneccessary?
   startTxt: `
-    press 'q' to quit
-    press 'Opt-Up' to jump to top
-    press 'Opt-Down' to jump to bottom
-    press 'Opt-Right' to jump to end of line
-    press 'Opt-Left' to jump to beginning of line
+
+    [keys]
+    
+    + 'q' quit
+    + 'S' save cursor position
+    + 'R' restore cursor position
+    + [enter] newline
+    
+    'Opt-[arrow keys]' jump
+
+    + 'Up' top of screen
+    + Down' bottom of screen
+    + 'Right' end of line
+    + 'Left' beginning of line
   `
 }
-
-// tput sc, rc
 
 const keyMap = {
   '\x03': 'Ctrl-C',
   '\x04': 'Ctrl-D',
   '\x06': 'Ctrl-F',
-  
+
   '\x0E': 'Ctrl-N',
   '\x1B[110;6u': 'Ctrl-Shift-N',
-  
+
   '\x13': 'Ctrl-S',
   '\x1B[115;6u': 'Ctrl-Shift-S',
-  
+
   '\x11': 'Ctrl-Q',
   '\x1B[113;6u': 'Ctrl-Shift-Q',
-  
+
   '\x17': 'Ctrl-W',
   '\x1B[119;6u': 'Ctrl-Shift-W',
-  
+
   '\x1A': 'Ctrl-Z',
   '\x1B[122;6u': 'Ctrl-Shift-Z',
-  
+
   '\x7F': 'Backspace',
-  '\r': 'Return',
+
+  '\r': env.newline,
   
-  '\t': 'Tab',
+  '\t': env.tab,
   '\x1B[Z': 'Shift-Tab',
 
   '\x1B': 'Ctrl-[',
@@ -68,21 +85,24 @@ const keyMap = {
 
   // '\x1B[A': 'Up',
   '\x1B[1;5A': 'Ctrl-Up',
-  // '\x1B[1;3A': 'Opt-Up',
   '\x1B[1;3A': env.gotoTop,
   '\x1B[1;2A': 'Shift-Up',
   '\x1B[1;6A': 'Ctrl-Shift-Up',
   '\x1B[1;4A': 'Opt-Shift-Up',
   '\x1B[1;8A': 'Ctrl-Opt-Shift-Up',
-  
+
   // '\x1B[B': 'Down',
   '\x1B[1;5B': 'Ctrl-Down',
-  // '\x1B[1;3B': 'Opt-Down',
   '\x1B[1;3B': env.gotoBottom,
   '\x1B[1;2B': 'Shift-Down',
   '\x1B[1;6B': 'Ctrl-Shift-Down',
   '\x1B[1;4B': 'Opt-Shift-Down',
-  '\x1B[1;8B': 'Ctrl-Opt-Shift-Down'
+  '\x1B[1;8B': 'Ctrl-Opt-Shift-Down',
+
+  '\x1B\b': env.jumpBackspace,
+
+  'S': env.sc,
+  'R': env.rc
 
   // ...
 }
@@ -90,15 +110,14 @@ const keyMap = {
 process.stdin.resume()
 process.stdin.setRawMode(true)
 
-process.stdin.on('data', x => {
+process.stdin.on('data', (x) => {
   const str = Buffer.from(x).toString()
 
   if (str === env.quitKey) process.exit(0)
 
   if (keyMap[str]) {
     process.stdout.write(keyMap[str])
-  }
-  else process.stdout.write(str)
+  } else process.stdout.write(str)
   // else console.log({str})
 })
 
@@ -108,5 +127,5 @@ process.on('SIGWINCH', () => {
   console.log(`SIGWINCH: ${env.maxWidth}, ${env.maxHeight}`)
 })
 
-process.on('exit', code => console.log(`quit with code: ${code}`))
+process.on('exit', code => { console.log(`quit with code: ${code}`) })
 console.log(env.startTxt)
